83b830cfe163e95b2f1055b0dc313941e30f8b4d
Nick Honings <409398@student.fontys.nl>
2020-06-11 11:01:49 +0200

    minor fixes

--- src/main/java/com/example/biometricauthenticationrest/REST/REST/MacRest.java
+++ src/main/java/com/example/biometricauthenticationrest/REST/REST/MacRest.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 @RestController
 @RequestMapping("/mac")
